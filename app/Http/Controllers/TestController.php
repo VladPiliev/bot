380f9005951656a6315670b7b2676f32c40d1d7c
VladPiliev <vladik.ardon@gmail.com>
2022-01-28 12:07:05 +0300

    q1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class TestController extends Controller
 {
     public function FunctionName()
     {
-        return response()->json("Vse ok!");
+        Http::post('https://api.tlgr.org/bot5111107809:AAGuxAaKiRnHna9wnAWSqh62QRQJP-fKP2E/sendMessage',[
+            'chat_id' => 778399078,
+            'text' => 'Hi'
+        ])
     }
 }
